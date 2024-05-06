de1986eb19481b8e96f94cf3ab137a054a75a39e
Brayan Sanjuan <bryan.sj175@gmail.com>
2024-05-06 10:47:39 -0500

    feat: :tada: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Styles
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 className="text-4xl text-center text-blue-500">
+      Hello, Tailwind CSS!
+    </h1>
+  </React.StrictMode>,
+)
